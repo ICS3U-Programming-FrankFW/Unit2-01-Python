48e9e3e03133731895adf2719624f84af059b138
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-22 12:13:32 +0000

    added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# Created By: Your Frankie fox wagoosh
+# Date: Sept 22, 2022
+# This program calculates the area and perimeter of a circle with a radius of 15mm. 
+import math 
+rad = 15
+circ = math.pi*2*rad
+area = math.pi*rad**2
+print(circ, area)
+9.24 , 706.8
+math.sqrt(100)
+10.0
+math.sqrt(25)
+5.0
